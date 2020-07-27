8562840debffe491491301d58197fc6bab35ec19
Tomos Fearn <tomos@blade.local>
2020-07-27 21:13:15 +0100

    updated interface

--- scripts/wheelchair_interface.py
+++ scripts/wheelchair_interface.py
@@ -1,16 +1,26 @@
-#import roslib
-#import os, sys
-#import rospy, rospkg
-#from std_msgs.msg import String
-#from std_msgs.msg import Float32
+#!/usr/bin/env python
 
-def main():
-    print("Hello World!")
-    print("------------")
+import rospy
+import os
+import sys
+from std_msgs.msg import String
 
-    print("Please add an instruction")
-    instruction = input()
-    print(f'you entered {instruction}')
+
+def interfaceNode():
+    pub = rospy.Publisher('rtabmapcoordinatesfrominterface', String, queue_size=10)
+    rospy.init_node('talker', anonymous=True)
+    rate = rospy.Rate(10) # 10hz
+    while not rospy.is_shutdown():
+        #print("Please add an instruction")
+        #instruction = input()
+        #print(f'you entered {instruction}')
+        hello_str = "hello world %s" % rospy.get_time()
+        rospy.loginfo(hello_str)
+        pub.publish(hello_str)
+        rate.sleep()
 
 if __name__ == "__main__":
-    main()
+    try:
+        interfaceNode()
+    except rospy.ROSInterruptException:
+        pass
