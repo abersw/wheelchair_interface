f4eba5c82c4f9a474d513bbb37701dbe1af9fce4
Tomos Fearn <tfearn10@outlook.com>
2020-07-28 15:46:41 +0100

    added subscriber

--- scripts/wheelchair_interface.py
+++ scripts/wheelchair_interface.py
@@ -5,18 +5,22 @@ import os
 import sys
 from std_msgs.msg import String
 
+def callback(data):
+    rospy.loginfo(rospy.get_caller_id() + " I heard %s", data.data)
 
 def interfaceNode():
-    pub = rospy.Publisher('rtabmapcoordinatesfrominterface', String, queue_size=10)
-    rospy.init_node('talker', anonymous=True)
+    publishGoal = rospy.Publisher('rtabmapcoordinatesfrominterface', String, queue_size=10)
+    subscribeInstruction = rospy.Subscriber("userinstructions", String, callback)
+    rospy.init_node('wheelchair_interface', anonymous=True)
     rate = rospy.Rate(10) # 10hz
     while not rospy.is_shutdown():
         #print("Please add an instruction")
         #instruction = input()
         #print(f'you entered {instruction}')
+        
         hello_str = "hello world %s" % rospy.get_time()
         rospy.loginfo(hello_str)
-        pub.publish(hello_str)
+        publishGoal.publish(hello_str)
         rate.sleep()
 
 if __name__ == "__main__":
