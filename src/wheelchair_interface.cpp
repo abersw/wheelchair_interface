cc01fd9d634d6796a96c8c0f93a7e96179772478
Tomos Fearn <tof7@aber.ac.uk>
2021-04-09 16:22:30 +0100

    added user request logic

--- src/wheelchair_interface.cpp
+++ src/wheelchair_interface.cpp
@@ -14,7 +14,13 @@
 #include "std_msgs/String.h"
 using namespace std;
 
+const bool DEBUG_requestUserInput = 0;
+const bool DEBUG_main = 0;
+
 ros::Publisher *ptr_publish_espeak;
+
+int wheelchair_interface_state = 1;
+std::string userInstructionRaw;
 
 /**
  * Function to shutdown ROS node safely
@@ -42,7 +48,9 @@ std::string requestUserInput() {
     std::string getUserInstructionRaw; //initialise variable for user input
     getline(std::cin, getUserInstructionRaw); //read line from user interface
     transform(getUserInstructionRaw.begin(), getUserInstructionRaw.end(), getUserInstructionRaw.begin(), ::tolower); //transform string to lower case letters
-
+    if (DEBUG_requestUserInput) {
+        cout << "user input is " << getUserInstructionRaw << endl; //return user input
+    }
     return getUserInstructionRaw; //return user input
 }
 
@@ -57,6 +65,26 @@ int main(int argc, char * argv[]) {
     ros::Rate loop_rate(10);
 
     while (ros::ok()) {
+
+        switch(wheelchair_interface_state) {
+            case 0: 
+                shutdownROSnode();
+                break;
+            case 1:
+                //get user instruction
+                userInstructionRaw = requestUserInput(); //request the user's input
+                if (userInstructionRaw != "") { //if user instruction is not blank
+                    wheelchair_interface_state = 2; //go to state 2 for publishing room name
+                }
+                break;
+            case 2:
+                if (DEBUG_main) {
+                    cout << userInstructionRaw << endl; //return room name
+                }
+                //publish user instruction function; //publish user instruction as ROS topic
+                wheelchair_interface_state = 1;
+                break;
+        }
 
         ros::spinOnce();
         loop_rate.sleep();
