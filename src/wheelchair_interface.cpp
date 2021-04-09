13845e9624dab616d9e7a743116c3d01089c5c3d
Tomos Fearn <tof7@aber.ac.uk>
2021-04-09 16:08:39 +0100

    added file info to top of file, added shutdown function

--- src/wheelchair_interface.cpp
+++ src/wheelchair_interface.cpp
@@ -1,9 +1,26 @@
+/*
+ * wheelchair_interface.cpp
+ * wheelchair_interface
+ * version: 0.0.1 Majestic Maidenhair
+ * Status: Pre-Alpha
+ * 
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sstream>
 #include "ros/ros.h" //main ROS library
 using namespace std;
+
+/**
+ * Function to shutdown ROS node safely
+ */
+void shutdownROSnode() {
+    cout << "shutting down ROS node" << endl;
+    ros::shutdown();
+    exit(0);
+}
 
 
 int main(int argc, char * argv[]) {
